5954f5d94ec7567f8db3fead763456986378ed77
Luiz Eduardo <duduamaralspereira@gmail.com>
2024-12-27 14:17:54 -0300

    Criação do arquivo stylesResult.js para os estilos da tela ResultScreen.js

---
+++ src/styles/stylesResult.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    view:{
+      flex: 0,
+      margin: 20
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color:"#008000"
+    },
+    result:{
+      alignItems: "center"
+    },
+    text: {
+      color: "#fff",
+      fontSize: 24,
+      fontWeight: "bold",
+    },
+    btn2: {
+      fontSize: 20,
+      borderColor: "#FF4500",
+      backgroundColor: "#363636",
+      borderWidth: 2,  
+      borderRadius: 8,
+      padding: 10, 
+    },
+    btnText: {
+      color: "#fff",
+      fontSize: 18
+    }
+  });
+  
